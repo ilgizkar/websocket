c1569ce5377b20073caece99d80f9480bcf561bf
ILGIZ <karabaev.il@yandex.ru>
2022-01-04 17:39:11 +0300

    First

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const express = require( "express");
+const WebSocket = require( "ws");
+const app = express();
+const server = http.createServer(app);
+const webSocketServer = new WebSocket.Server({ server });
+
+const dispatchEvent = (message, ws) => {
+    const json = JSON.parse(message);
+    console.log(json)
+    switch (json.event) {
+        case "chat-message":
+            webSocketServer.clients.forEach(client => client.send(JSON.stringify(json)));
+            break;
+
+        default:
+            ws.send((new Error("Wrong query")).message);
+    }
+}
+
+webSocketServer.on('connection', ws => {
+    ws.on('message', m => dispatchEvent(m, ws));
+    ws.on("error", e => ws.send(e));
+
+    ws.send('Hi there, I am a WebSocket server');
+});
+
+server.listen(3000, () => console.log("Server started"))
